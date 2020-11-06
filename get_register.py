d960d6a086104b236c82b8e3d4d724361f1eef66
Ton Kuijt <github@kuijt.nu>
2020-11-06 09:03:29 +0100

    Update get_register.py
    De libraries zijn intussen gewijzigd, en python2 is deprecated dus script aangepast.

--- get_register.py
+++ get_register.py
@@ -3,18 +3,21 @@
 # Import needed libraries
 import sys
 import minimalmodbus
-
-
-# Set minimalmodbus to the serial parameters for your device
-minimalmodbus.BAUDRATE=9600
-minimalmodbus.PARITY='E'
+import serial
 
 # Declare an instrument (device) to be queried
-instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1) # port name, slave address (in decimal)
+instrument = minimalmodbus.Instrument('/dev/ttyUSB1', 1) # port name, slave address (in decimal)
+
+# Settings for the given instrument
+instrument.serial.baudrate = 9600		# Baud rate
+instrument.serial.bytesize = 8			# Data bits
+instrument.serial.parity = serial.PARITY_EVEN	# Parity
+instrument.serial.stopbits = 1			# Stop bits
+instrument.mode = minimalmodbus.MODE_RTU	# Device mode
 
 ## Read register value given on cli
 register = int(sys.argv[1])
 value = instrument.read_float(register, 3, 2) # Registernumber, number of decimals
 
 # Return found register value
-print(value) 
+print(value)
